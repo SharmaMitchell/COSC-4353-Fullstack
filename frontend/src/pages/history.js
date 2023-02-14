3e2c2d982565c4974f6d63af98ff68de597479f9
Mitchell Sharma <sharmamitch@gmail.com>
2023-02-14 00:10:22 -0600

    Implemented section title component for pages and sections

--- frontend/src/pages/history.js
+++ frontend/src/pages/history.js
@@ -1,13 +1,18 @@
 import React from "react";
 import DataTable from '../components/DataTable/DataTable'
+import  SectionTitle from '../components/SectionTitle/SectionTitle'
 
 const History = props => {
     return(
-        <div style={{width: 100 + '%', display: 'flex', justifyContent: "center"}}>
-            <div style={{maxWidth: 1300 + 'px',  marginTop: 100 + 'px'}}>
-                <DataTable />
+        <>
+            <SectionTitle text="Estimate History" />
+            <div style={{width: 100 + '%', display: 'flex', justifyContent: "center"}}>
+                <div style={{maxWidth: 1300 + 'px'}}>
+                    <DataTable />
+                </div>
             </div>
-        </div>
+        </>
+        
     )
 };
 
