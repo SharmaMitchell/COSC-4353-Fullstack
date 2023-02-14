834ffc74bf5b5e15b623099bdef7d9b6e33322b3
Mitchell Sharma <sharmamitch@gmail.com>
2023-02-13 18:33:08 -0600

    Added placeholder routing, defined font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,14 @@ import History from "./pages/history.js";
 import Home from "./pages/home.js";
 import Login from "./pages/login.js";
 import ManageProfile from "./pages/manage-profile.js";
+import Navbar from './components/Navbar/Navbar.js'
 
 function App() {
   return (
     <div className="App">
+      <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
       <Router>
+      <Navbar />
         <Routes> 
           <Route path='/' element={<Home/>} />
           <Route path='/history' element={<History/>} />
