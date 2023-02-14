ecd5769316634b5c61350caddf719b541c663bfc
Mitchell Sharma <sharmamitch@gmail.com>
2023-02-13 23:57:41 -0600

    Implemented table component for history page using Material UI

--- frontend/src/pages/history.js
+++ frontend/src/pages/history.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import DataTable from '../components/DataTable/DataTable'
 
 const History = props => {
     return(
-        <div>
-        <p>History</p>
+        <div style={{width: 100 + '%', display: 'flex', justifyContent: "center"}}>
+            <div style={{maxWidth: 1300 + 'px',  marginTop: 100 + 'px'}}>
+                <DataTable />
+            </div>
         </div>
     )
 };
