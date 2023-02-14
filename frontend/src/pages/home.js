92167ff4667018075128fba7f770ec6ee96249b0
Clara-Martin <110358276+Clara-Martin@users.noreply.github.com>
2023-02-13 19:39:38 -0600

    Login and Home start

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,10 +1,27 @@
 import React from "react";
+import CityImage from "../images/city-skyline.jpg"
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
 
 
 const Home = props => {
+    const navigate = useNavigate();
+    const getEstimate = () => {
+        navigate('/estimate');
+      };
+
     return(
         <div>
-        <p>Home</p>
+            <div style={{backgroundImage: `url(${CityImage})`,position: "relative", backgroundRepeat:"no-repeat", backgroundSize:"cover", width:"100%", height:"700px"}}>
+            <button type="button" onClick={getEstimate} style={{position:"fixed", top:"600px",left: "50%",transform: "translate(-50%, -50%)",backgroundColor:"#fd6600"}}> GET STARTED </button>
+            <a href="#overview" style={{position:"fixed", top:"625px",left: "50%",transform: "translate(-50%, -50%)", color:"white"}}>Learn More</a>
+            </div>
+            <div id="overview">
+                <h1 style={{color:"#2b3a55"}}>OVERVIEW</h1>
+            </div>
+            <div id="features">
+                <h1 style={{color:"#2b3a55"}}>FEATURES</h1>
+            </div>
         </div>
     )
 };
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -4,7 +4,25 @@ import React from "react";
 const Login = props => {
     return(
         <div>
-        <p>Login</p>
+            <h1>LOG IN</h1>
+            
+            <div className="row pb-1">
+                {/*username input box*/}
+                <div className="input-group col-lg-4">
+                    <label for="date_time">Username:</label>
+                    <input type="text" placeholder="Username, ex: ProGamer123"/>
+                </div>
+
+                {/*password input box*/}
+                <div className="input-group col-lg-4">
+                    <label for="date_time">Password:</label>
+                    <input type="text" placeholder="*******"/>
+                </div>
+
+                <div className="input-group-append">
+                    <button type="button"> LOG IN </button>
+                </div>
+            </div>
         </div>
     )
 };
