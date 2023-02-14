bf0fedfc1ba27ceaa9165532476e8758e2bc62a0
Clara-Martin <110358276+Clara-Martin@users.noreply.github.com>
2023-02-13 18:14:47 -0600

    Pages/Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Estimate from "./pages/estimate.js";
+import History from "./pages/history.js";
+import Home from "./pages/home.js";
+import Login from "./pages/login.js";
+import ManageProfile from "./pages/manage-profile.js";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes> 
+          <Route path='/' element={<Home/>} />
+          <Route path='/history' element={<History/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/estimate' element={<Estimate/>} />
+          <Route path='/manage-profile' element={<ManageProfile/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
