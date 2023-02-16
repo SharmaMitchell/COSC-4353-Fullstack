ced09e564681f4409baa61f4b84fe201105009b4
Clara-Martin <110358276+Clara-Martin@users.noreply.github.com>
2023-02-16 17:58:48 -0600

    Page Skeletons

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,19 +1,13 @@
 import React from "react";
 import CityImage from "../images/city-skyline.jpg"
-import {Routes, Route, useNavigate} from 'react-router-dom';
 
 
 
 const Home = props => {
-    const navigate = useNavigate();
-    const getEstimate = () => {
-        navigate('/estimate');
-      };
-
     return(
         <div>
             <div style={{backgroundImage: `url(${CityImage})`,position: "relative", backgroundRepeat:"no-repeat", backgroundSize:"cover", width:"100%", height:"700px"}}>
-            <button type="button" onClick={getEstimate} style={{position:"fixed", top:"600px",left: "50%",transform: "translate(-50%, -50%)",backgroundColor:"#fd6600"}}> GET STARTED </button>
+            <button type="button" style={{position:"fixed", top:"600px",left: "50%",transform: "translate(-50%, -50%)",backgroundColor:"#fd6600"}}> <a href="/estimate">GET STARTED </a></button>
             <a href="#overview" style={{position:"fixed", top:"625px",left: "50%",transform: "translate(-50%, -50%)", color:"white"}}>Learn More</a>
             </div>
             <div id="overview">
