9204a532e40d9252bc05e2b1b9370ee75eadeced
Mitchell Sharma <sharmamitch@gmail.com>
2023-02-18 17:32:21 -0600

    Fixed centered homepage logo :)

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -9,9 +9,10 @@ import Banner from "../assets/logo-no-background.png";
 const Home = props => {
     return(
         <div>
-            <div style={{backgroundImage: `url(${CityImage})`, boxShadow: 'inset 0 0 0 1000px rgba(0,0,0,.2)', position: "relative", backgroundRepeat:"no-repeat", backgroundSize:"cover", width:"100%", height:"700px",bottom:"50px"}}>
+            <div style={{backgroundImage: `url(${CityImage})`, display: 'flex', alignItems: 'center', justifyContent: 'center', boxShadow: 'inset 0 0 0 1000px rgba(0,0,0,.2)', position: "relative", backgroundRepeat:"no-repeat", backgroundSize:"cover", width:"100%", height:"700px",bottom:"50px"}}>
                 {/* TODO: add text/banner to image (see design, room for improvement if you have ideas) */}
-                <div style={{backgroundImage: `url(${Banner})`, position: "absolute", backgroundRepeat:"no-repeat", margin:"auto", width:"75%", height:"50%", top:"30%"}}></div>
+                {/* <div style={{backgroundImage: `url(${Banner})`, position: "absolute", backgroundRepeat:"no-repeat", margin:"auto", width:"75%", height:"50%", top:"30%"}}></div> */}
+                <img src={Banner} style={{position:"fixed", maxWidth: "90vw"}}/>
                 <Button variant="contained" type="button" size="large" style={{position:"fixed", top:"600px",left: "50%",transform: "translate(-50%, -50%)",backgroundColor:"#fd6600"}}> <a href="/estimate" style={{color:'white'}}>GET STARTED </a></Button>
                 <a href="#overview" style={{textDecoration:"underline", position:"fixed", top:"635px",left: "50%",transform: "translate(-50%, -50%)", color:"white"}}>Learn More</a>
             </div>
