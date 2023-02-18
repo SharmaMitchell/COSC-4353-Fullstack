7879b43beeb436075156cb8a0961d6384cf4bce7
Clara-Martin <110358276+Clara-Martin@users.noreply.github.com>
2023-02-18 16:23:46 -0600

    oil banner

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import CityImage from "../assets/Houston-skyline.jpg"
 import  SectionTitle from '../components/SectionTitle/SectionTitle'
 import Button from '@mui/material/Button';
+import Banner from "../assets/logo-no-background.png";
 
 
 
@@ -10,6 +11,7 @@ const Home = props => {
         <div>
             <div style={{backgroundImage: `url(${CityImage})`, boxShadow: 'inset 0 0 0 1000px rgba(0,0,0,.2)', position: "relative", backgroundRepeat:"no-repeat", backgroundSize:"cover", width:"100%", height:"700px",bottom:"50px"}}>
                 {/* TODO: add text/banner to image (see design, room for improvement if you have ideas) */}
+                <div style={{backgroundImage: `url(${Banner})`, position: "absolute", backgroundRepeat:"no-repeat", left:"12.5%", width:"75%", height:"50%"}}></div>
                 <Button variant="contained" type="button" size="large" style={{position:"fixed", top:"600px",left: "50%",transform: "translate(-50%, -50%)",backgroundColor:"#fd6600"}}> <a href="/estimate" style={{color:'white'}}>GET STARTED </a></Button>
                 <a href="#overview" style={{textDecoration:"underline", position:"fixed", top:"635px",left: "50%",transform: "translate(-50%, -50%)", color:"white"}}>Learn More</a>
             </div>
