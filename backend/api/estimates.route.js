9ee9a7678ce9ec14e52f9a46e1326a38ca902d86
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-02 13:53:04 -0600

    Implemented history endpoint and mock data response

--- backend/api/estimates.route.js
+++ backend/api/estimates.route.js
@@ -1,9 +1,12 @@
 import express from "express"
-
-//get access to express router
+import fs from "fs"
 const router = express.Router()
 
 //demo route
 router.route("/").get((req, res) => res.send("hello world"))
+router.route("/history/:clientID").get((req, res) => {
+    const mockData = JSON.parse(fs.readFileSync("api/mockData/example-data.json"))
+    res.send(mockData)
+})
 
 export default router
