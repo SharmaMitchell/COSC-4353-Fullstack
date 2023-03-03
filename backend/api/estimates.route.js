38b6bc1dca44d227d62b71d4f64115b9e4919e96
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-02 20:52:35 -0600

    Updated history/estimates route

--- backend/api/estimates.route.js
+++ backend/api/estimates.route.js
@@ -1,8 +1,11 @@
 import express from "express"
-
+import fs from "fs"
 const router = express.Router()
 
-//demo route
 router.route("/").get((req, res) => res.send("hello world"))
+router.route("/:clientID").get((req, res) => {
+    const mockData = JSON.parse(fs.readFileSync("api/mockData/example-data.json"))
+    res.send(mockData)
+})
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@
 import express from "express";
 import cors from "cors";
 import estimates from "./api/estimates.route.js";
-import history from "./api/history.route.js";
 
 const app = express();
 
@@ -13,8 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 //for app.use() middleware function is mounted to path specified
-app.use("/api/v1", estimates)
-app.use("/api/v1/history", history)
+app.use("/api/v1/estimates", estimates)
 //if a link not specified is visited, return an error
 //here, req is the HTTP request the server receives and res is the HTTP response it returns
 //in this case the response is an HTTP error code (404) and a json file stating "not found"
