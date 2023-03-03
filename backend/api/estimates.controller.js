bc4eac2ab6aec787b2676008ad25eb076ee9b01a
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-02 20:53:07 -0600

    Defined estimates DAO and UpdateEstimate API call

---
+++ backend/api/estimates.controller.js
@@ -0,0 +1,29 @@
+import EstimatesDAO from "../dao/estimatesDAO.js"
+
+export default class EstimatesController {
+    static async apiUpdateEstimates(req, res, next) {
+        try {
+            const user = req.body.client_ID
+            const estimateDate = req.body.estimate_date
+            const gallonsRequested = req.body.gallons_requested
+            const deliveryAddress = req.body.address
+            const deliveryDate = req.body.delivery_date
+            const suggestedPrice = req.body.suggested_price
+            const quote = req.body.quote
+
+            const EstimatesResponse = await EstimatesDAO.updateEstimates(
+                user,
+                estimateDate,
+                gallonsRequested,
+                deliveryAddress,
+                deliveryDate,
+                suggestedPrice,
+                quote
+            )
+            res.json({ status: "success" })
+        } 
+        catch (err) {
+            res.status(500).json({ error: err.message })    
+        }
+  }
+}
--- backend/dao/estimatesDAO.js
+++ backend/dao/estimatesDAO.js
@@ -1,21 +1,36 @@
-//create a variable the stores a reference to the database
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
+
 let estimates
 
-export default class EstimatesDAO{
-    //define method to initially connect to database
-        //called as soon as server starts
-        //get reference to database
-    static async injectDB(conn){
-        //if already have a reference
-        if (estimates){
+export default class estimatesDAO {
+    static async injectDB(conn) {
+        if(estimates){
             return
         }
-        //if not, try to connect
         try {
-            estimates = await conn.db(process.env.ESTIMATES_NS).collection("estimates")
+            estimates = await conn.db(process.env.estimates_NS).collection("estimates")
+        } catch (e) {
+            console.error(`Unable to establish collection handles in estimatesDAO: ${e}`)
         }
-        catch(err) {
-            console.error(`Unable to establish connection to database in estimatesDAO: ${err}`)
+    }
+    // Add estimate to database
+    static async addEstimate(user, estimateDate, gallons, address, deliveryDate, suggestedPrice, quote) {
+        try {
+            const estimateDoc = {
+                user: user,
+                estimateDate: estimateDate,
+                gallonsRequested: gallons,
+                deliveryAddress: address,
+                deliveryDate: deliveryDate,
+                suggestedPrice: suggestedPrice,
+                quote: quote
+            }
+            return await estimates.insertOne(estimateDoc)
+        } catch (e) {
+            console.error(`Unable to post estimate: ${e}`)
+            return { error: e }
         }
     }
 }
+
