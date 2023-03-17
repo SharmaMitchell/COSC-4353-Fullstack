a212b72cb070b0423f3114a7a6670c6e7b44d94c
Clara-Martin <110358276+Clara-Martin@users.noreply.github.com>
2023-03-17 01:36:14 -0500

    profile data populating profile management form fields

--- frontend/src/pages/manage-profile.js
+++ frontend/src/pages/manage-profile.js
@@ -14,16 +14,16 @@ import Stack from '@mui/material/Stack';
 
 
 const ManageProfile = (props) => {
-    const [name, setName] = useState('');
-    const [address, setAddress] = useState('');
-    const [address2, setAddress2] = useState('');
-    const [city, setCity] = useState('');
-    const [state, setState] = useState('');
-    const [zipcode, setZipcode] = useState('');
+    const [name, setName] = useState(null);
+    const [address, setAddress] = useState(null);
+    const [address2, setAddress2] = useState(null);
+    const [city, setCity] = useState(null);
+    const [state, setState] = useState("");
+    const [zipcode, setZipcode] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (address == ''){
+        if (address == null){
             getValues(props.logID)
         }
     });
@@ -85,47 +85,97 @@ const ManageProfile = (props) => {
                 <form onSubmit={handleSubmit}>
                     <Grid container spacing = {2}  >
                         <Grid item xs = {12}>
-                            <TextField  
-                            label="Full Name" 
-                            id = "name"
-                            placeholder = "John Doe"
-                            inputProps = {{maxLength:50}}
-                            required
-                            onChange={(event) => setName(event.target.value)}
-                            />
+                            {name == null ? 
+                                <TextField   
+                                label="Full Name" 
+                                id = "name"
+                                placeholder = "John Doe"
+                                inputProps = {{maxLength:50}}
+                                helperText="Some important text"
+                                required
+                                onChange={(event) => setName(event.target.value)}
+                                />
+                                : 
+                                <TextField
+                                label="Full Name" 
+                                id = "name"
+                                value = {name}
+                                inputProps = {{maxLength:50}}
+                                required
+                                onChange={(event) => setName(event.target.value)}
+                                InputLabelProps={{ shrink: true }}
+                                />
+                            }
                         </Grid>
                         <Grid item xs = {12} direction = {"row"}>
-                            <TextField  
-                            label="Street Address 1" 
-                            id = "address"
-                            placeholder = "9813 Nut Street"
-                            inputProps = {{maxLength:100}}
-                            required
-                            onChange={(event) => setAddress(event.target.value)}
-                            />
+                            {address == null ?
+                                <TextField  
+                                    label="Street Address 1" 
+                                    id = "address"
+                                    placeholder = "9813 Nut Street"
+                                    inputProps = {{maxLength:100}}
+                                    required
+                                    onChange={(event) => setAddress(event.target.value)}
+                                />
+                                :
+                                <TextField  
+                                    label="Street Address 1" 
+                                    id = "address"
+                                    value = {address}
+                                    inputProps = {{maxLength:100}}
+                                    required
+                                    onChange={(event) => setAddress(event.target.value)}
+                                    InputLabelProps={{ shrink: true }}
+                                />
+                            }
+
                         </Grid>
                         <Grid item xs = {12} direction = {'row'}>
-                            <TextField  
-                            label="Street Address 2" 
-                            id = "address2"
-                            placeholder = "Apt, Suite, etc... "
-                            inputProps = {{maxLength:100}}
-                            onChange={(event) => setAddress2(event.target.value)}
-                            />
+                            {address2 == null ?
+                                <TextField  
+                                label="Street Address 2" 
+                                id = "address2"
+                                placeholder = "Apt, Suite, etc... "
+                                inputProps = {{maxLength:100}}
+                                onChange={(event) => setAddress2(event.target.value)}
+                                />
+                                :
+                                <TextField  
+                                label="Street Address 2" 
+                                id = "address2"
+                                value = {address2}
+                                inputProps = {{maxLength:100}}
+                                onChange={(event) => setAddress2(event.target.value)}
+                                InputLabelProps={{ shrink: true }}
+                                />
+                            }
                         </Grid>
                         <Grid item xs = {12} direction = {'column'} >
-                            <TextField  
-                                label="City" 
-                                id = "city"
-                                placeholder = "Houston"
-                                inputProps = {{maxLength:100}}
-                                required
-                                onChange={(event) => setCity(event.target.value)}
+                            {city == null ?
+                                <TextField  
+                                    label="City" 
+                                    id = "city"
+                                    placeholder = "Houston"
+                                    inputProps = {{maxLength:100}}
+                                    required
+                                    onChange={(event) => setCity(event.target.value)}
                                 />
+                                :
+                                <TextField  
+                                    label="City" 
+                                    id = "city"
+                                    value = {city}
+                                    inputProps = {{maxLength:100}}
+                                    required
+                                    onChange={(event) => setCity(event.target.value)}
+                                    InputLabelProps={{ shrink: true }}
+                                />
+                            }
                         </Grid>
                         <Grid item xs = {12} direction = {'row'} >
                             <FormControl sx = {{width: 225,}} required>
                                 <InputLabel id = "demo-simple-select-label">State</InputLabel>
+                                {state == null ?
                                 <Select
                                     labelId = "demo-simple-select-label"
                                     label = "State"
@@ -133,7 +183,6 @@ const ManageProfile = (props) => {
                                     sx={{textAlign: 'left',}}
                                     onChange={(event) => setState(event.target.value)}
                                 >   
-                                    <MenuItem value={''}>Select a State...</MenuItem>
                                     <MenuItem value={'AL'}>Alabama</MenuItem>
                                     <MenuItem value={'AK'}>Alaska</MenuItem>
                                     <MenuItem value={'AZ'}>Arizona</MenuItem>
@@ -186,17 +235,91 @@ const ManageProfile = (props) => {
                                     <MenuItem value={"WI"}>Wisconsin</MenuItem>
                                     <MenuItem value={"WY"}>Wyoming</MenuItem>
                                 </Select>
+                                :
+                                <Select
+                                labelId = "demo-simple-select-label"
+                                label = "State"
+                                required
+                                value = {state}
+                                sx={{textAlign: 'left',}}
+                                onChange={(event) => setState(event.target.value)}
+                            >   
+                                <MenuItem value={'AL'}>Alabama</MenuItem>
+                                <MenuItem value={'AK'}>Alaska</MenuItem>
+                                <MenuItem value={'AZ'}>Arizona</MenuItem>
+                                <MenuItem value={'AR'}>Arkansas</MenuItem>
+                                <MenuItem value={'CA'}>California</MenuItem>
+                                <MenuItem value={'CO'}>Colorado</MenuItem>
+                                <MenuItem value={'CT'}>Connecticut</MenuItem>
+                                <MenuItem value={'DE'}>Delaware</MenuItem>
+                                <MenuItem value={'DC'}>District Of Columbia</MenuItem>
+                                <MenuItem value={'FL'}>Florida</MenuItem>
+                                <MenuItem value={'GA'}>Georgia</MenuItem>
+                                <MenuItem value={'HI'}>Hawaii</MenuItem>
+                                <MenuItem value={'ID'}>Idaho</MenuItem>
+                                <MenuItem value={'IL'}>Illinois</MenuItem>
+                                <MenuItem value={'IN'}>Indiana</MenuItem>
+                                <MenuItem value={'IA'}>Iowa</MenuItem>
+                                <MenuItem value={'KS'}>Kansas</MenuItem>
+                                <MenuItem value={'KY'}>Kentucky</MenuItem>
+                                <MenuItem value={'LA'}>Louisiana</MenuItem>
+                                <MenuItem value={'ME'}>Maine</MenuItem>
+                                <MenuItem value={'MD'}>Maryland</MenuItem>
+                                <MenuItem value={'MA'}>Massachusetts</MenuItem>
+                                <MenuItem value={'MI'}>Michigan</MenuItem>
+                                <MenuItem value={'MN'}>Minnesota</MenuItem>
+                                <MenuItem value={'MS'}>Mississippi</MenuItem>
+                                <MenuItem value={'MO'}>Missouri</MenuItem>
+                                <MenuItem value={'MT'}>Montana</MenuItem>
+                                <MenuItem value={'NE'}>Nebraska</MenuItem>
+                                <MenuItem value={'NV'}>Nevada</MenuItem>
+                                <MenuItem value={"NH"}>New Hampshire</MenuItem>
+                                <MenuItem value={"NJ"}>New Jersey</MenuItem>
+                                <MenuItem value={"NM"}>New Mexico</MenuItem>
+                                <MenuItem value={"NY"}>New York</MenuItem>
+                                <MenuItem value={"NC"}>North Carolina</MenuItem>
+                                <MenuItem value={"ND"}>North Dakota</MenuItem>
+                                <MenuItem value={"OH"}>Ohio</MenuItem>
+                                <MenuItem value={"OK"}>Oklahoma</MenuItem>
+                                <MenuItem value={"OR"}>Oregon</MenuItem>
+                                <MenuItem value={"PA"}>Pennsylvania</MenuItem>
+                                <MenuItem value={"RI"}>Rhode Island</MenuItem>
+                                <MenuItem value={"SC"}>South Carolina</MenuItem>
+                                <MenuItem value={"SD"}>South Dakota</MenuItem>
+                                <MenuItem value={"TN"}>Tennessee</MenuItem>
+                                <MenuItem value={"TX"}>Texas</MenuItem>
+                                <MenuItem value={"UT"}>Utah</MenuItem>
+                                <MenuItem value={"VT"}>Vermont</MenuItem>
+                                <MenuItem value={"VA"}>Virginia</MenuItem>
+                                <MenuItem value={"WA"}>Washington</MenuItem>
+                                <MenuItem value={"WV"}>West Virginia</MenuItem>
+                                <MenuItem value={"WI"}>Wisconsin</MenuItem>
+                                <MenuItem value={"WY"}>Wyoming</MenuItem>
+                            </Select>
+                            }
                             </FormControl>
                         </Grid>
                         <Grid item xs = {12} direction = {'row'}>
-                            <TextField  
-                                label="Zipcode" 
-                                type="number"
-                                placeholder = "77055"
-                                inputProps = {{minLength: 5, maxLength:9}}
-                                required
-                                onChange={(event) => setZipcode(event.target.value)}
+                            {zipcode == null ?
+                                <TextField  
+                                    label="Zipcode" 
+                                    type="number"
+                                    placeholder = "77055"
+                                    inputProps = {{minLength: 5, maxLength:9}}
+                                    required
+                                    onChange={(event) => setZipcode(event.target.value)}
                                 />
+                                :
+                                <TextField  
+                                    label="Zipcode" 
+                                    type="number"
+                                    value = {zipcode}
+                                    inputProps = {{minLength: 5, maxLength:9}}
+                                    required
+                                    onChange={(event) => setZipcode(event.target.value)}
+                                    InputLabelProps={{ shrink: true }}
+                                />
+                            }
                         </Grid>
                         <Grid item xs = {12} direction = {'row'}>
                             <Button type="submit" variant="contained" color="primary">SAVE</Button>
