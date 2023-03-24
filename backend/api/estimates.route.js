a0f2bd41ebc82d0a600ddf67b4e052899c8d61ea
Devon Chao <devon_chao@yahoo.com>
2023-03-23 19:31:47 -0500

    routes for get and save

--- backend/api/estimates.route.js
+++ backend/api/estimates.route.js
@@ -11,5 +11,7 @@ router.route("/manage-profile").put(ProfileCtrl.apiUpdateProfile)
 router.route("/get-profile").get(ProfileCtrl.apiGetProfileData)
 router.route("/register").post(ProfileCtrl.apiCreateProfile)
 router.route("/login").post(ProfileCtrl.apiLoginProfile)
+router.route("/get-estimate").get((req, res) => res.send("hello world")); 
+router.route("/save-estimate").post((req, res) => res.send("hello world")); 
 
 export default router;
