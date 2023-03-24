6fc3602db1dc644de83bd263aadcec2c38cb40f6
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-23 20:30:41 -0500

    Fixed estiamtes api endpoint

--- backend/api/estimates.route.js
+++ backend/api/estimates.route.js
@@ -6,12 +6,17 @@ import ProfileCtrl from "./profile.controller.js";
 const router = express.Router();
 
 //demo route
-router.route("/estimates").get((req, res) => res.send("hello world"))
-router.route("/manage-profile").put(ProfileCtrl.apiUpdateProfile)
-router.route("/get-profile").get(ProfileCtrl.apiGetProfileData)
-router.route("/register").post(ProfileCtrl.apiCreateProfile)
-router.route("/login").post(ProfileCtrl.apiLoginProfile)
-router.route("/get-estimate").get((req, res) => res.send("hello world")); 
-router.route("/save-estimate").post((req, res) => res.send("hello world")); 
+// router.route("/:clientID").get((req, res) => {
+//     const mockData = JSON.parse(fs.readFileSync("api/mockData/example-data.json"))
+//     res.send(mockData)
+// })
+router.route("/:clientID").get(estimatesController.apiGetEstimates);
+router.route("/:clientID").post(estimatesController.apiUpdateEstimates);
+router.route("/manage-profile").put(ProfileCtrl.apiUpdateProfile);
+router.route("/get-profile").get(ProfileCtrl.apiGetProfileData);
+router.route("/register").post(ProfileCtrl.apiCreateProfile);
+router.route("/login").post(ProfileCtrl.apiLoginProfile);
+router.route("/get-estimate").get((req, res) => res.send("hello world"));
+router.route("/save-estimate").post((req, res) => res.send("hello world"));
 
 export default router;
--- frontend/src/components/DataTable/DataTable.js
+++ frontend/src/components/DataTable/DataTable.js
@@ -1,49 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 // import tableData from './example-data.json'
 
 function DataTable(props) {
   // const USER_ID = props.userID
-  const USER_ID = "63f82d40be153fa3c4b62062"
-  const [tableData, setTableData] = useState([])
+  const USER_ID = "63f82d40be153fa3c4b62062";
+  const [tableData, setTableData] = useState([]);
   useEffect(() => {
-    fetch(`http://localhost:5000/api/v1/estimates/${USER_ID ? USER_ID : ""}`)
-    .then((data) => {
-      return data.json()
-    })
-    .then((data) => {
-      setTableData(data.estimates)
-      console.log(data.estimates)
-      return tableData
-    })
-  },[])
+    fetch(`http://localhost:5000/api/v1/${USER_ID ? USER_ID : ""}`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        setTableData(data.estimates);
+        console.log(data.estimates);
+        return tableData;
+      });
+  }, []);
   const headStyle = {
-    fontWeight: 600
-  }
+    fontWeight: 600,
+  };
   const StyledTableRow = styled(TableRow)(() => ({
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: `var(--card-background)`,
     },
   }));
   function formatTime(date) {
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let period = hours < 12 ? 'AM' : 'PM';
+    let period = hours < 12 ? "AM" : "PM";
     hours = hours % 12 || 12;
     minutes = minutes < 10 ? `0${minutes}` : minutes;
     return `${hours}:${minutes}${period}`;
   }
   const formatDate = (dateString) => {
     let theDate = new Date(dateString);
-    return `${theDate.getMonth() + 1}/${theDate.getDate()}/${theDate.getFullYear()}, ${formatTime(theDate)}`;
-  }
+    return `${
+      theDate.getMonth() + 1
+    }/${theDate.getDate()}/${theDate.getFullYear()}, ${formatTime(theDate)}`;
+  };
   return (
     <TableContainer component={Paper}>
       <Table aria-label="estimate history table">
@@ -61,20 +63,29 @@ function DataTable(props) {
           {tableData.map((row, index) => (
             <StyledTableRow
               key={index}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell>{formatDate(row.estimateDate)}</TableCell>
-              <TableCell>{row.gallonsRequested.toLocaleString('en-US')}</TableCell>
+              <TableCell>
+                {row.gallonsRequested.toLocaleString("en-US")}
+              </TableCell>
               <TableCell>{row.deliveryAddress}</TableCell>
               <TableCell>{formatDate(row.deliveryDate)}</TableCell>
-              <TableCell>${parseFloat(row.suggestedPrice).toFixed(2).toLocaleString('en-US')}</TableCell>
-              <TableCell>${parseFloat(row.quote).toFixed(2).toLocaleString('en-US')}</TableCell>
+              <TableCell>
+                $
+                {parseFloat(row.suggestedPrice)
+                  .toFixed(2)
+                  .toLocaleString("en-US")}
+              </TableCell>
+              <TableCell>
+                ${parseFloat(row.quote).toFixed(2).toLocaleString("en-US")}
+              </TableCell>
             </StyledTableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
-  )
+  );
 }
 
-export default DataTable
+export default DataTable;
